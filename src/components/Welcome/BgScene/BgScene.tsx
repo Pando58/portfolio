a6800fe89c657d55c98f611760fbdfb864fdf378
Enrique Pando <enr.pando@gmail.com>
2023-01-31 17:05:18 -0600

    Scale the camera view when resizing

--- src/components/Welcome/BgScene/BgScene.tsx
+++ src/components/Welcome/BgScene/BgScene.tsx
@@ -1,5 +1,5 @@
 import { useMouseScreenPosition } from "@/components/hooks/useMouseScreenPosition";
-import { PerspectiveCamera } from "@react-three/drei";
+import { Bounds, PerspectiveCamera } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
   BrightnessContrast,
@@ -103,55 +103,56 @@ function BgSceneContent() {
           <meshStandardMaterial color={"#404048"} />
         </mesh>
       ))}
-
-      <Blob
-        radius={3}
-        pos={[4, -1.6, -3.8]}
-        u_noiseDensity={1}
-        u_noiseStrength={0.4}
-        u_frequency={14}
-        u_amplitude={2}
-        u_colorIntensity={0.8}
-        u_colorOffset={0.4}
-        u_colorPalette={[
-          [0.2, 0.7, 0.4], // [0.2, 0.7, 0.4],
-          [0.6, 0.9, 0.3], // [0.6, 0.9, 0.2],
-          [0.5, 0.8, 1.0], // [0.6, 0.8, 0.7],
-          [0.5, 0.1, 0.0], // [0.5, 0.1, 0.0],
-        ]}
-      ></Blob>
-      <Blob
-        radius={1}
-        pos={[-3.7, 1, 1]}
-        u_noiseDensity={1}
-        u_noiseStrength={0.2}
-        u_frequency={10}
-        u_amplitude={2}
-        u_colorIntensity={2.4}
-        u_colorOffset={-2}
-        u_colorPalette={[
-          [0.2, 0.7, 0.4],
-          [0.6, 0.9, 0.3],
-          [0.6, 0.8, 0.7],
-          [0.5, 0.1, 0.0],
-        ]}
-      ></Blob>
-      <Blob
-        radius={0.3}
-        pos={[1.2, 2, 1.6]}
-        u_noiseDensity={1}
-        u_noiseStrength={0.04}
-        u_frequency={20}
-        u_amplitude={2}
-        u_colorIntensity={8}
-        u_colorOffset={-2.2}
-        u_colorPalette={[
-          [0.3, 0.6, 0.4],
-          [0.6, 0.9, 0.3],
-          [0.6, 0.8, 0.7],
-          [0.5, 0.1, 0.0],
-        ]}
-      ></Blob>
+      <Bounds fit clip observe damping={6} margin={0.7}>
+        <Blob
+          radius={3}
+          pos={[4, -1.6, -3.8]}
+          u_noiseDensity={1}
+          u_noiseStrength={0.4}
+          u_frequency={14}
+          u_amplitude={2}
+          u_colorIntensity={0.8}
+          u_colorOffset={0.4}
+          u_colorPalette={[
+            [0.2, 0.7, 0.4], // [0.2, 0.7, 0.4],
+            [0.6, 0.9, 0.3], // [0.6, 0.9, 0.2],
+            [0.5, 0.8, 1.0], // [0.6, 0.8, 0.7],
+            [0.5, 0.1, 0.0], // [0.5, 0.1, 0.0],
+          ]}
+        ></Blob>
+        <Blob
+          radius={1}
+          pos={[-3.7, 1, 1]}
+          u_noiseDensity={1}
+          u_noiseStrength={0.2}
+          u_frequency={10}
+          u_amplitude={2}
+          u_colorIntensity={2.4}
+          u_colorOffset={-2}
+          u_colorPalette={[
+            [0.2, 0.7, 0.4],
+            [0.6, 0.9, 0.3],
+            [0.6, 0.8, 0.7],
+            [0.5, 0.1, 0.0],
+          ]}
+        ></Blob>
+        <Blob
+          radius={0.3}
+          pos={[1.2, 2, 1.6]}
+          u_noiseDensity={1}
+          u_noiseStrength={0.04}
+          u_frequency={20}
+          u_amplitude={2}
+          u_colorIntensity={8}
+          u_colorOffset={-2.2}
+          u_colorPalette={[
+            [0.3, 0.6, 0.4],
+            [0.6, 0.9, 0.3],
+            [0.6, 0.8, 0.7],
+            [0.5, 0.1, 0.0],
+          ]}
+        ></Blob>
+      </Bounds>
     </>
   );
 }
